e90ccbd7bb2dab5e17fb1d63462a7b2edfa3c4cc
jungwoo0719 <jungwoo07191@naver.com>
2025-06-12 18:16:30 +0900

    굿

--- 1100/1451.c
+++ 1100/1451.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    
+    int a, b[100001]={0,},temp=0;
+    scanf("%d", &a);
+    for(int i=1; i<=a; i++){
+        scanf("%d", &b[i]);
+    }
+    for(int i=1; i<a; i++){
+        for(int j=i+1; j<=a; j++){
+            if(b[i]>b[j]){
+                temp = b[i];
+                b[i]=b[j];
+                b[j]=temp;
+            }
+        }
+    }
+    for(int i=1; i<=a; i++){
+        printf("%d\n", b[i]);
+    }
+    return 0;
 }
